9fdf50e84136bbd1babb5870e9555ebb1de4af76
EgorZhi <vern1992@yandex.ru>
2022-03-20 01:58:11 +0300

    menu burger

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,19 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
 */
+
+// Меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.header__menu');
+if (iconMenu) {
+	iconMenu.addEventListener("click", function (e) {
+		// Добавляем класс _active для body для того чтобы
+		// убрать возможность прокрутки сайт при открытом меню "бургер"
+		document.body.classList.toggle('_lock');
+		// Добавляем класс _active для menu__icon для ее анимирования
+		iconMenu.classList.toggle('_active');
+		// Добавляем класс _active для header__menu для анимирования
+		// состояния появления и исчезания блока
+		menuBody.classList.toggle('_active');
+	});
+}
